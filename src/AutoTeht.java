2e3908eca45ae60394945ba4ae1a57c60127b384
roosa22017 <roosa22017@student.hamk.fi>
2022-12-13 17:53:51 +0200

    Commit 1

--- src/AutoTeht.java
+++ src/AutoTeht.java
@@ -2,8 +2,37 @@
 public class AutoTeht {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 }
+
+class Auto {
+	//ominaisuudet
+	public String merkki;
+	public String malli;
+	public int bensanMaara;
+	
+	public void jarruta()
+	{
+		System.out.println("Auto jarruttaa.");
+	}
+	
+	public void kiihdyta()
+	{
+		if (bensanMaara > 0)
+		{	
+		System.out.println("Auto kiihtyy.");
+		bensanMaara = bensanMaara-1;
+		}
+	}
+	
+	public void naytaTiedot()
+	{
+		System.out.println("Auto on " + merkki + " " +  malli + " ja bensaa on jäjellä " +  bensanMaara);
+	}
+	
+	
+	}
+	
